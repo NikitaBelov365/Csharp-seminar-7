2ffc386ae1aee0919a666b83d31a435ccf7db57c
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-04 18:30:16 +0300

    task 3 solved

---
+++ task 3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 2: Задайте двумерный массив. 
+// Найдите элементы, у которых обе позиции чётные, 
+// и замените эти элементы на их квадраты.
+
+
+int InputSize(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] ArrayCreation(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return array;
+}
+
+void ArrayChanging(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i%2-1 == 0 && j%2-1 == 0) array[i, j] *= array[i, j];
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int rows = InputSize("Input ammount of rows");
+int columns = InputSize("Input ammount of columns");
+int[,] array = ArrayCreation(rows, columns);
+PrintArray(array);
+ArrayChanging(array);
+PrintArray(array);
