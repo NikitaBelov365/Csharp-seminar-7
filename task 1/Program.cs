ae43b965564033315db5f5cf15d6f7b99cf0c872
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-04 17:46:43 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+
+int InputSize(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] ArrayCreation(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+
+int rows = InputSize("Input ammount of rows");
+int columns = InputSize("Input ammount of columns");
+PrintArray(ArrayCreation(rows, columns));
